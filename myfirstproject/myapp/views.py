86581705a510fb7717eb97867d3269a45fdbe916
widy228 <antonovv@internet.ru>
2025-12-07 15:49:39 +0300

    /

--- myfirstproject/myapp/views.py
+++ myfirstproject/myapp/views.py
@@ -9,13 +9,3 @@ def about(request):
 def contact(request):
     return HttpResponse('<h2>Контакты</h2>')
 
-def products(request, productid):
-    category = request.GET.get('cat', '')
-    output = '<h2>Продукт № {0} Category: {1}</h2>'.format(productid, category)
-    return HttpResponse(output)
-
-def users(request):
-    id = request.GET.get('id', 1)
-    name = request.GET.get('name', 'Tom')
-    output = '<h2>User</h2><h3>id: {0} name: {1}</h3>'.format(id, name)
-    return HttpResponse(output)
